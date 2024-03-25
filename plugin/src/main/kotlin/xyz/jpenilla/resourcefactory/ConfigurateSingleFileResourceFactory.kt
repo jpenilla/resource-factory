54d90e49a9b01d4afaa6e4120c75a0a12f0b7055
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-03-25 10:55:29 -0700

    Count loader factory/constant values as task inputs
    This means if a user changes their config from `yaml()` to `yaml { println("a") }` for example, the implementation change will be detected and the task re-run.

--- plugin/src/main/kotlin/xyz/jpenilla/resourcefactory/ConfigurateSingleFileResourceFactory.kt
+++ plugin/src/main/kotlin/xyz/jpenilla/resourcefactory/ConfigurateSingleFileResourceFactory.kt
@@ -2,7 +2,6 @@ package xyz.jpenilla.resourcefactory
 
 import org.gradle.api.Action
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Nested
 import org.spongepowered.configurate.ConfigurationNode
 import org.spongepowered.configurate.gson.GsonConfigurationLoader
@@ -10,22 +9,42 @@ import org.spongepowered.configurate.loader.ConfigurationLoader
 import org.spongepowered.configurate.yaml.YamlConfigurationLoader
 import xyz.jpenilla.resourcefactory.util.nullAction
 import java.nio.file.Path
+import java.util.function.Function
 import javax.inject.Inject
 
 abstract class ConfigurateSingleFileResourceFactory : SingleFileResourceFactory() {
-    @get:Internal
-    abstract val loaderFactory: Property<(Path) -> ConfigurationLoader<*>>
+    @get:Nested
+    abstract val loaderFactory: Property<Function<Path, ConfigurationLoader<*>>>
+
+    // These helper methods look overcomplicated at first, but this is necessary to
+    // have the value/configuration (implementation and nested properties) provided by the user count as task inputs.
 
     fun yaml(configure: Action<YamlConfigurationLoader.Builder> = nullAction()) {
-        loaderFactory.set { path -> YamlConfigurationLoader.builder().also { configure.execute(it) }.path(path).build() }
+        loaderFactory.set(object : Function<Path, ConfigurationLoader<*>> {
+            @get:Nested
+            val configure: Action<YamlConfigurationLoader.Builder> = configure
+
+            override fun apply(path: Path): ConfigurationLoader<*> = YamlConfigurationLoader.builder()
+                .also { this.configure.execute(it) }
+                .path(path)
+                .build()
+        })
     }
 
     fun json(configure: Action<GsonConfigurationLoader.Builder> = nullAction()) {
-        loaderFactory.set { path -> GsonConfigurationLoader.builder().also { configure.execute(it) }.path(path).build() }
+        loaderFactory.set(object : Function<Path, ConfigurationLoader<*>> {
+            @get:Nested
+            val configure: Action<GsonConfigurationLoader.Builder> = configure
+
+            override fun apply(path: Path): ConfigurationLoader<*> = GsonConfigurationLoader.builder()
+                .also { this.configure.execute(it) }
+                .path(path)
+                .build()
+        })
     }
 
     override fun generateSingleFile(outputFile: Path) {
-        val loader = loaderFactory.get()(outputFile)
+        val loader = loaderFactory.get().apply(outputFile)
         loader.save(generateRootNode(loader))
     }
 
@@ -36,7 +55,14 @@ abstract class ConfigurateSingleFileResourceFactory : SingleFileResourceFactory(
         abstract val value: Property<ValueProvider>
 
         fun value(value: Any) {
-            this.value.set { value }
+            this.value.set(object : ValueProvider {
+                @get:Nested
+                val value = value
+
+                override fun asConfigSerializable(): Any {
+                    return this.value
+                }
+            })
         }
 
         override fun <N : ConfigurationNode> generateRootNode(loader: ConfigurationLoader<N>): N {
