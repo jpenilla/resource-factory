def0d1b73e3bad3ea314097fc41d0952cdcb72f9
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-03-25 11:13:18 -0700

    Fail on duplicate single file factories

--- plugin/src/main/kotlin/xyz/jpenilla/resourcefactory/SingleFileResourceFactory.kt
+++ plugin/src/main/kotlin/xyz/jpenilla/resourcefactory/SingleFileResourceFactory.kt
@@ -1,5 +1,6 @@
 package xyz.jpenilla.resourcefactory
 
+import org.gradle.api.GradleException
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import java.nio.file.Path
@@ -8,9 +9,31 @@ abstract class SingleFileResourceFactory : ResourceFactory {
     @get:Input
     abstract val path: Property<String>
 
+    @get:Input
+    abstract val duplicatesMode: Property<DuplicatesMode>
+
+    enum class DuplicatesMode {
+        IGNORE,
+        FAIL
+    }
+
+    init {
+        init()
+    }
+
+    private fun init() {
+        duplicatesMode.convention(DuplicatesMode.FAIL)
+    }
+
     abstract fun generateSingleFile(outputFile: Path)
 
     override fun generate(outputDir: Path) {
-        generateSingleFile(outputDir.resolve(path.get()))
+        val outputFile = outputDir.resolve(path.get())
+        if (duplicatesMode.get() == DuplicatesMode.FAIL) {
+            if (outputFile.toFile().exists()) {
+                throw GradleException("File '$outputFile' already exists, did another factory create it?")
+            }
+        }
+        generateSingleFile(outputFile)
     }
 }
